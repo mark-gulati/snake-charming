b3d298922552c067e3155190a8b3aa42a0ed76ca
mark-gulati <kgl_m@yahoo.com>
2018-08-06 17:55:35 +0200

    twitter work

--- twitter-twatter.py
+++ twitter-twatter.py
@@ -12,7 +12,13 @@ twitter = Twython(twitter_credentials['consumer_api_key'],
   twitter_credentials['access_token'],
   twitter_credentials['token_secret'])
 
-results = twitter.search(q = "pylint")
-
-print(type(results))
-print(results)
+results = twitter.search(q = "demi lovato")
+#'screen_name': 'pythonvscode'
+print(type(results)) # it is a <class 'dict'>
+i = 0
+print("Number of posts found: ", len(results['statuses']))
+for i in range(0,len(results['statuses'])):
+    print(results['statuses'][i]['entities'])
+    print(results['statuses'][i]['entities']['urls'])
+twitter.update_status(status="Nice day today...")
+print("Exiting...")
